d44b17a92fc79ca40f7f9ac90d75d0937dbd1037
Davit808 <arzumanyan.davit.92@gmail.com>
2023-01-06 22:26:58 +0400

    Add Homework part 5, Delete command "break"

--- src/Part2.java
+++ src/Part2.java
@@ -5,46 +5,35 @@ public class Part2 {
         Scanner number = new Scanner(System.in);
         System.out.println("enter the number of month");
         int i = number.nextInt();
-        switch (i){
+        if (0 < i && i < 13){
+        switch (i) {
             case 1:
                 System.out.println(i + " is January - 31 day");
-                break;
             case 2:
                 System.out.println(i + " is February - 28 day");
-                break;
             case 3:
                 System.out.println(i + " is March - 31 day");
-                break;
             case 4:
                 System.out.println(i + " is April - 30 day");
-                break;
             case 5:
                 System.out.println(i + " is May - 31 day");
-                break;
             case 6:
                 System.out.println(i + " is June - 30 day");
-                break;
             case 7:
                 System.out.println(i + " is July - 31 day");
-                break;
             case 8:
                 System.out.println(i + " is August - 31 day");
-                break;
             case 9:
                 System.out.println(i + " is September - 30 day");
-                break;
             case 10:
                 System.out.println(i + " is October - 31 day");
-                break;
             case 11:
                 System.out.println(i + " is November - 30 day");
-                break;
             case 12:
                 System.out.println(i + " is December - 31 day");
-                break;
-            default:
-                System.out.println("Year is not have a Month in " + i);
-
+        }
+        } else {
+            System.out.println("Year is not have a Month in " + i);
         }
 
     }
