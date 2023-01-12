be865f52b64856984dc76939f2a492c76021b264
Davit808 <arzumanyan.davit.92@gmail.com>
2023-01-13 00:42:13 +0400

    Add search a letter on the array

---
+++ src/charArray.java
@@ -0,0 +1,18 @@
+public class charArray {
+    public static void main(String[] args) {
+        char[] text = {'h', 'e', 'l', 'l', 'o', ' ', 'b', 'r', 'o'};
+        char letter = 'o';
+        int i = 0;
+        while (i< text.length){
+            if (text[i] == letter) {
+                break;
+            }
+            i++;
+        }
+        if(i == text.length && text[i-1] != letter){
+            System.out.println("latter " + letter + " is not found");
+        }else {
+            System.out.println("index " + letter + " is " + i);
+        }
+    }
+}
