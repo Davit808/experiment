61b89472e0712c346f32f98414d76be9366078a7
Davit808 <arzumanyan.davit.92@gmail.com>
2023-01-13 00:40:10 +0400

    Add negative module

---
+++ src/moduleArray.java
@@ -0,0 +1,18 @@
+public class moduleArray {
+    public static void main(String[] args) {
+        int[] array = {-6, 2, 11, 6, -21, 9};
+        int x = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0) {
+                array[i] = -1 * array[i];
+                x++;
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Negative is " + x);
+        System.out.println("Positive is " + (array.length - x));
+    }
+}
