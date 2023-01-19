026baa0339977e3cb4f8892287d8d193fe2f568b
Davit808 <arzumanyan.davit.92@gmail.com>
2023-01-20 01:02:40 +0400

    Add prime numbers "New version"

--- src/Arrays/PrimeNumbers.java
+++ src/Arrays/PrimeNumbers.java
@@ -2,18 +2,33 @@ package Arrays;
 
 public class PrimeNumbers {
     public static void main(String[] args) {
-        int[] arr = {0, 5, 6, 21, 0, 7, 3, 13, 34};
-
+        int[] arr = {6, 21, 3, 34, 5, 7};
+        boolean count = false;
         for (int i = 0; i < arr.length; i++) {
-            boolean count = true;
-            for (int j = 2; j <= arr[i] / 2; j++) {
+            for (int j = 2; j <= arr[i] / 2 + 1; j++) {
                 if (arr[i] % j == 0) {
-                    count = false;
+                    break;
+                } else if (j > arr[i] / 2) {
+                    System.out.print(arr[i] + " ");
+                    count = true;
                 }
             }
-            if (count != false && arr[i] != 0) {
-                System.out.print(arr[i] + " ");
-            }
+        }
+        if (count == false) {
+            System.out.println("This array isn't having a prime numbers");
         }
     }
 }
+
+//        for (int i = 0; i < arr.length; i++) {
+//            boolean count = true;
+//            for (int j = 2; j <= arr[i] / 2; j++) {
+//                if (arr[i] % j == 0) {
+//                    count = false;
+//                }
+//            }
+//            if (count1 != false && arr[i] != 0) {
+//                System.out.print(arr[i] + " ");
+//            }
+//        }
+
