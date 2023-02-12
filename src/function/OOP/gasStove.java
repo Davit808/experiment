df0798b66b20fb1f5cbbba29ef80eb75a6689fe9
Davit808 <arzumanyan.davit.92@gmail.com>
2023-02-12 21:58:43 +0400

    Add my stove

--- src/function/OOP/gasStove.java
+++ src/function/OOP/gasStove.java
@@ -2,9 +2,8 @@ package function.OOP;
 
 public class gasStove {
     public static void main(String[] args) {
-        gasStoveConstructor gs1 = new gasStoveConstructor(false, true); // N1 is electric eye, N2 is gas eye
+        gasStoveConstructor gs1 = new gasStoveConstructor();
+     //   gs1.setPlayEye(false, false);// N1 is electric eye, N2 is gas eye
         System.out.println(gs1.totalEnergy);
-        System.out.println(gs1.gasEye1 + " " + gs1.gasEye2 + " " + gs1.gasEye3);
-        System.out.println(gs1.elEye1 + " " + gs1.elEye2);
     }
 }
--- src/function/OOP/gasStoveConstructor.java
+++ src/function/OOP/gasStoveConstructor.java
@@ -1,20 +1,27 @@
 package function.OOP;
 
 public class gasStoveConstructor {
-    int gasEye1, gasEye2, gasEye3;
-    int elEye1, elEye2;
+    private int gasEye1, gasEye2, gasEye3;
+    private int elEye1, elEye2;
+    private int sumElEnergy;
+    private int sumGasEnergy;
     int totalEnergy;
-    gasStoveConstructor(boolean p1, boolean p2){
-        boolean playElectric = p1;
-        boolean playGas = p2;
+    private boolean playElectric;
+    private boolean playGas;
+    public void setPlayEye(boolean p1, boolean p2){
+        this.playElectric = p1;
+        this.playGas = p2;
         if (p1){
             elEye1 = elEye2 = 100;
+            sumElEnergy = elEye1 + elEye2;
         }
-        int totalElectricEnergy = elEye1 + elEye2;
-        if(p2){
+        if (p2){
             gasEye1 = gasEye2 = gasEye3 = 80;
+            sumGasEnergy = gasEye1 + gasEye2 + gasEye3;
         }
-        int totalGasEnergy = gasEye1 + gasEye2 + gasEye3;
-        totalEnergy = totalElectricEnergy + totalGasEnergy;
+        totalEnergy = sumElEnergy + sumGasEnergy;
+    }
+    public int getTotalEnergy(){
+        return totalEnergy;
     }
 }
