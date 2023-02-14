0bec613493304d859475e208e624c52150cc6984
Davit808 <arzumanyan.davit.92@gmail.com>
2023-02-14 14:21:10 +0400

    Add sportsman result

--- src/function/OOP/Sportsman.java
+++ src/function/OOP/Sportsman.java
@@ -1,9 +1,9 @@
 package function.OOP;
 
-public class sportsman {
+public class Sportsman {
     String name;
     int result;
-    sportsman(String n,int...p) {
+    Sportsman(String n, int...p) {
        name = n;
         int[] points = p;
         result = resultTotalPoints(points);
@@ -19,9 +19,9 @@ public class sportsman {
     }
 
     public static void main(String[] args) {
-        sportsman human1 = new sportsman("Muhammad Ali", 99, 98, 95, 100, 95, 50);
-        sportsman human2 = new sportsman("Mike Tyson", 95, 97, 90, 98, 98, 90);
-        sportsman human3 = new sportsman("Roy Jones", 85, 88, 95, 99, 90);
+        Sportsman human1 = new Sportsman("Muhammad Ali", 99, 98, 95, 100, 95, 50);
+        Sportsman human2 = new Sportsman("Mike Tyson", 95, 97, 90, 98, 98, 90);
+        Sportsman human3 = new Sportsman("Roy Jones", 85, 88, 95, 99, 90);
         int[] sportsmanResult = {human1.result, human2.result, human3.result};
         for (int a : sportsmanResult) {
             System.out.print(a + " ");
