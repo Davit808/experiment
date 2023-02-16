3a527aaf85c966a33deb8b434281afe17a6c61ae
Davit808 <arzumanyan.davit.92@gmail.com>
2023-02-17 02:37:57 +0400

    Add comparison two sportsman

---
+++ src/function/OOP/Comparison.java
@@ -0,0 +1,21 @@
+package function.OOP;
+
+public class Comparison {
+    private int compareTheResults;
+    private int resultOfSportsman1;
+    private int resultOfSportsman2;
+
+    Comparison(int sportsman1, int sportsman2) {
+        resultOfSportsman1 = sportsman1;
+        resultOfSportsman2 = sportsman2;
+    }
+
+    public int getCompareTheResults() {
+        if (resultOfSportsman1 > resultOfSportsman2) {
+            return compareTheResults = 1;
+        } else if (resultOfSportsman1 < resultOfSportsman2) {
+            return compareTheResults = -1;
+        }
+        return compareTheResults = 0;
+    }
+}
