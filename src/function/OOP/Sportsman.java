142f257fe9ba337528dd22808934bdcdb4c84578
Davit808 <arzumanyan.davit.92@gmail.com>
2023-02-19 12:53:26 +0400

    Add this and other sportsman result

--- src/function/OOP/Sportsman.java
+++ src/function/OOP/Sportsman.java
@@ -2,45 +2,44 @@ package function.OOP;
 
 public class Sportsman {
     private String name;
-    private int numberOfLength;
-    private int[] allPointsForSportsman = new int[numberOfLength];
+    private int[] competitionResults;
     private int result;
 
-    Sportsman(String n, int... p) {
+    Sportsman(String n, int... CR) {
         name = n;
-        setNumberOfLength(p.length);
-        int[] allPoints = new int[numberOfLength];
-        for (int i = 0; i < p.length; i++) {
-            for (int j = 0; j < p.length; j++) {
-                if (p[j] < 50) {
-                    allPoints[j] = 50;
-                } else if (p[j] > 100) {
-                    allPoints[j] = 100;
-                } else {
-                    allPoints[j] = p[j];
-                }
+        for (int i = 0; i < CR.length; i++) {
+            if (CR[i] < 50) {
+                CR[i] = 50;
+            } else if (CR[i] > 100) {
+                CR[i] = 100;
             }
         }
-        allPointsForSportsman = allPoints;
-        result = resultTotalPoints();
+        competitionResults = CR;
+        result = result();
     }
 
-    private int resultTotalPoints() {
+    public int[] getCompetitionResults() {
+        return competitionResults;
+    }
+
+    private int result() {
         int sum = 0;
-        for (int i = 0; i < allPointsForSportsman.length; i++) {
-            sum += allPointsForSportsman[i];
+        for (int i = 0; i < competitionResults.length; i++) {
+            sum += competitionResults[i];
         }
-        return sum / allPointsForSportsman.length;
-    }
-
-    public void setNumberOfLength(int x) {
-        if (x < 6) {
-            this.numberOfLength = 6;
-        }
-        this.numberOfLength = x;
+        return sum / competitionResults.length;
     }
 
     public int getResult() {
         return result;
+    }
+
+    int resultThisAndAnotherSportsman(Sportsman anotherSportsmanResult) {
+        if (this.result > anotherSportsmanResult.result) {
+            return 1;
+        } else if (this.result < anotherSportsmanResult.result) {
+            return -1;
+        }
+        return 0;
     }
 }
