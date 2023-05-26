959a4489267ac896b496aba9241187da7329f956
Davit808 <arzumanyan.davit.92@gmail.com>
2023-05-26 17:10:25 +0400

    Add methods of ArrayList

--- src/OOP/MyArrayList.java
+++ src/OOP/MyArrayList.java
@@ -1,8 +1,43 @@
 package OOP;
+
 import java.util.ArrayList;
 import java.lang.Number;
+import java.util.LinkedList;
 
 public class MyArrayList {
+    public static void main(String[] args) {
+        // Question 1
+        ArrayList<String> arr = new ArrayList<>();
+        ArrayList<Integer> arr2 = new ArrayList<>(20);
+        Object o = new Object();
+        System.out.println(arr.equals(arr2)); //arr and arr2 have a instance specified type
+        System.out.println(arr2.equals(arr));//arr2 and arr have a instance specified type
+        System.out.println(arr.equals(o)); // o and arr haven't a instance specified tape and result is false. if one element is null them result instanceof is false
+        arr.add("test");
+        System.out.println(arr.equals(arr2));
+        LinkedList<String> a = new LinkedList<>();
+        LinkedList<Integer> b = new LinkedList<>();
+        System.out.println(a.equals(b));
+        a.add("test");
+        b.add(2);
+        System.out.println(a.equals(b));
+        // Question 2
+        for (int i = 0; i < 10; i++) {
+            arr2.add(i);
+        }
+        for (int i = 5; i >= 0; i--) {
+            arr2.add(i);
+        }
+        arr2.add(4, 0);
+        System.out.println(arr2);
+        for (int j = 1; j < arr2.size(); j++) {
+            if (arr2.get(0).equals(arr2.get(j))) {
+                arr2.remove(j);
+            }
+        }
+        System.out.println(arr2);
+    }
+//      Question 3
 //    public static <E> ArrayList<E> returnElements(ArrayList<E> arrayList) {
 //        ArrayList<E> sublist = new ArrayList<>();
 //        ArrayList<Number> sublist2 = new ArrayList<>();
@@ -17,33 +52,4 @@ public class MyArrayList {
 //        }
 //        return sublist;
 //    }
-    public static void main(String[] args) {
-        ArrayList<String> arr = new ArrayList<>();
-        ArrayList<Integer> arr2 = new ArrayList<>(20);
-        Object o = new Object();
-        System.out.println(arr.equals(arr2)); //arr and arr2 have a instance specified type
-        System.out.println(arr2.equals(arr));//arr2 and arr have a instance specified type
-        System.out.println(arr.equals(o)); // o and arr haven't a instance specified tape and result is false. if one element is null them result instanceof is false
-        for (int i = 0; i < 10; i++) {
-            arr2.add(i);
-        }
-        for (int i = 5; i >= 0; i--) {
-            arr2.add(i);
-        }
-        arr2.add(4, 0);
-        System.out.println(arr2);
-        for (int j = 1; j < arr2.size(); j++) {
-            if (arr2.get(0).equals(arr2.get(j))) {
-                arr2.remove(j);
-            }
-        }
-        arr2.remove(0);
-        System.out.println(arr2);
-//        ArrayList<String> arr3 = new ArrayList<>();
-//        arr3.add("test");
-//        arr3.add("test1");
-//        System.out.println(arr3);
-//        arr2.add(6, -5);
-//        System.out.println(returnElements(arr2));
-    }
 }
